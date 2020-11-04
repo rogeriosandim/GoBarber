91452e06a459405d1cdd1e0b071dc777bba208e2
Rogério Sandim <sandimroger@gmail.com>
2020-11-04 11:48:33 +0000

    feat: create SignIn & SignUp pages

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Login here</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Password" />
+
+      <Button onPress={() => {}}>Sign In</Button>
+    </Container>
+  );
+};
+
+export default SignIn;
