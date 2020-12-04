03f077eb9896a43981c9ba99803192ef25c6c9c5
Rogério Sandim <sandimroger@gmail.com>
2020-12-04 13:22:19 +0000

    fix: add await on tests that has errors

--- server/src/modules/users/services/AuthenticateUserService.spec.ts
+++ server/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -43,7 +43,7 @@ describe('AuthenticateUser', () => {
       fakeHashRepository
     );
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: '123456',
@@ -71,7 +71,7 @@ describe('AuthenticateUser', () => {
       password: '123456',
     });
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: 'wrong-password',
