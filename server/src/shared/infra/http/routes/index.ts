afb8caa63a6e54b60c433672c41f2996fc13d4b6
Rogério Sandim <sandimroger@gmail.com>
2020-12-05 16:46:29 +0000

    feat: create routes and controllers

--- server/src/shared/infra/http/routes/index.ts
+++ server/src/shared/infra/http/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
-import appointmentsRouters from '@modules/appointments/infra/http/routes/appointments.routes';
-import usersRouters from '@modules/users/infra/http/routes/users.routes';
-import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
+import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
-routes.use('/appointments', appointmentsRouters);
-routes.use('/users', usersRouters);
-routes.use('/sessions', sessionsRoutes);
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
