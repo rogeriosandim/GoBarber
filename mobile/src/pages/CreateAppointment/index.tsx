5c14d77be591e16107264d667c281ad02227d447
Rogério Sandim <sandimroger@gmail.com>
2020-12-30 11:05:11 +0000

    feat: create the create appointment page

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useAuth } from '../../hooks/auth';
+import { useRoute, useNavigation } from '@react-navigation/native';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const { user } = useAuth();
+  const route = useRoute();
+  const navigation = useNavigation();
+  const { providerId } = route.params as RouteParams;
+
+  return (
+    <>
+      <Header>
+        <BackButton onPress={() => navigation.goBack()}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Heirdressers</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+      <Container></Container>
+    </>
+  );
 };
 
 export default CreateAppointment;
